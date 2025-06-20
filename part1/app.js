40f74ff96c9180a82bf04812541ba86159bf56e4
Jacob Mancini <jmancini021@gmail.com>
2025-06-20 07:32:26 +0000

    [automatic commit] 2025-06-20 07:32:26 UTC 175.35.4.166

--- part1/app.js
+++ part1/app.js
@@ -79,6 +79,7 @@ let db;
 
     await db.execute(`
       CREATE TABLE IF NOT EXISTS WalkApplications (
+        application_id INT
         request_id INT AUTO_INCREMENT PRIMARY KEY,
         dog_id INT NOT NULL,
         requested_time DATETIME NOT NULL,
