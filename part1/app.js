c11b746bc196f5b3f9af795f74d2a3b8aa8ff855
Jacob Mancini <jmancini021@gmail.com>
2025-06-20 07:21:01 +0000

    [automatic commit] 2025-06-20 07:21:01 UTC 175.35.4.166

--- part1/app.js
+++ part1/app.js
@@ -138,7 +138,8 @@ let db;
             INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
             VALUES (
                 (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Bella')),
-                (SELECT user_id FROM Users WHERE username = ')
+                (SELECT user_id FROM Users WHERE username = 'evewalker'),
+                (SELECT )
             );
         `);
     }
