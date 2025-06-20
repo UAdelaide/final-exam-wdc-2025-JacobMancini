f317e89409aef30418f3683c0a007330797ad73c
Jacob Mancini <jmancini021@gmail.com>
2025-06-20 08:08:25 +0000

    [automatic commit] 2025-06-20 08:08:25 UTC 175.35.4.166

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -37,7 +37,7 @@ router.get('/walkrequests/open', async (req, res) => {
 router.get('/walkers/summary', async (req, res) => {
     try {
         const[rows] = await db.execute(`
-
+            SELECT 
         `);
         res.json(rows);
     } catch (err) {
