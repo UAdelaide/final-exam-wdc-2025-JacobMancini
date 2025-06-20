1b9e611eb5e1f3da7fa0cf7b7143085b4031b658
Jacob Mancini <jmancini021@gmail.com>
2025-06-20 06:54:49 +0000

    [automatic commit] 2025-06-20 06:54:49 UTC 175.35.4.166

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = expressRouter();
 
 let db;
-function 
+
 
 router.get('/dogs', async (req, res) => {
     try {
         const[rows] = await
+    }
+    catch {
+        
     }
 });
