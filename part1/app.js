0275ec2f11725bf00e54b0d94b86cf4b5c24547e
Jacob Mancini <jmancini021@gmail.com>
2025-06-20 07:27:04 +0000

    [automatic commit] 2025-06-20 07:27:04 UTC 175.35.4.166

--- part1/app.js
+++ part1/app.js
@@ -151,7 +151,7 @@ let db;
         await db.execute(`
             INSERT INTO WalkApplications (request_id, walker_id, status)
             VALUES (
-                
+                (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Max')),
             );
         `);
     }
