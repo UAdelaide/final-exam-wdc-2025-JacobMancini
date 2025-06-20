92e24b4be838ffcb1cd854d56c040bbd35cf156c
Jacob Mancini <jmancini021@gmail.com>
2025-06-20 07:55:38 +0000

    [automatic commit] 2025-06-20 07:55:38 UTC 175.35.4.166

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -11,8 +11,8 @@ router.get('/dogs', async (req, res) => {
         const[rows] = await db.execute(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
             FROM Dogs d
-            JOIN Users ON d.`
-        );
+            JOIN Users ON d.owner_id = u.user_id
+        `);
         res.json(rows);
     } catch (err) {
         res.status(500).json({ error: 'Could not get Dogs' });
