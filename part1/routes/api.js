2180eb8dd999ff57e6c8ede99205fd70c2446039
Jacob Mancini <jmancini021@gmail.com>
2025-06-20 07:53:58 +0000

    [automatic commit] 2025-06-20 07:53:58 UTC 175.35.4.166

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -9,7 +9,7 @@ function setDatabase(database) {
 router.get('/dogs', async (req, res) => {
     try {
         const[rows] = await db.execute(`
-            SELECT `
+            SELECT d.name AS dog_name, d.size, u.username AS owner_username`
         );
         res.json(rows);
     } catch (err) {
