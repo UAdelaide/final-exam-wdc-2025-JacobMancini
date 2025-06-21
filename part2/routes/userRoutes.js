f396558e53a8d289622cc804525b497ee90d505b
Jacob Mancini <jmancini021@gmail.com>
2025-06-21 04:06:31 +0000

    [automatic commit] 2025-06-21 04:06:31 UTC 175.35.4.166

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -69,21 +69,4 @@ router.post('/logout', (req, res) => {
   });
 });
 
-// Dog acquisition for select menu
-router.get('/name', async (req, res) => {
-  const owner_id = req.session.user.user_id;
-  const [dogs] = await db.query(`SELECT dog_id, name FROM Dogs WHERE owner_id = ?`, [owner_id]);
-  res.json(dogs);
-});
-
-// Dog acquisition for table images
-router.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT * FROM Dogs');
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
-});
-
 module.exports = router;
