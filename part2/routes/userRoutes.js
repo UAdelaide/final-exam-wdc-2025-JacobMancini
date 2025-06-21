4b374c4c075011df8e68062706dfc3f550b79ebe
Jacob Mancini <jmancini021@gmail.com>
2025-06-21 02:20:41 +0000

    [automatic commit] 2025-06-21 02:20:41 UTC 175.35.4.166

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -71,6 +71,7 @@ router.post('/logout', (req, res) => {
 
 // Dog acquisition for select menu
 router.get('/api/dogs/name', async (req, res) => {
+  const owner_id = req.session.user.user_id;
   
 });
 module.exports = router;
